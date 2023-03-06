7fd8c5cd722be79890e150f6546cf99b94a5a17d
Ilan Tervil <ilan.tervil@wanadoo.fr>
2023-03-06 17:33:24 +0100

    2e commit, first structure of code, with comment.

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 /** My JS Game */
 
-/** Function Game */
+/** Function Game 
+ * 1) Fonction de randomisation/aléatoire du jeté de dès.
+ * 2) Reset Game, Reinit, or begin a new game
+ * 3) Click on dice, for change player
+ * 4) Addition of score, hold, and if score >= to max, player winner, with win animation (css)
+ * 5) change the user
+ * 6) change total score
+ * 7) change dice (random image of dice)
+ * 8) put in place dice images with random function
+ * 9) ...
+*/
+/** Class Game 
+ * Manage the Game
+ * Class player
+ * Class dice
+*/
