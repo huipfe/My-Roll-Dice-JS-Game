01f12c4fd704c280d3c5a562c4bede3ae77e3c7f
Ilan Tervil <ilan.tervil@wanadoo.fr>
2023-03-10 17:43:03 +0100

    14e commit begin of code in JS, begin of game & Modal

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 /** My JS Game */
 
 /** Function Game 
- * 1) Fonction de randomisation/aléatoire du jeté de dès.
+ * 1) Randomization/random function of roll of a dice game.
  * 2) Reset Game, Reinit, or begin a new game
  * 3) Click on dice, for change player
  * 4) Addition of score, hold, and if score >= to max, player winner, with win animation (css)
@@ -9,10 +9,70 @@
  * 6) change total score
  * 7) change dice (random image of dice)
  * 8) put in place dice images with random function
- * 9) ...
+ * 9) ... Maybe ?
 */
+
 /** Class Game 
  * Manage the Game
  * Class player
  * Class dice
 */
+
+
+// Random Dice
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max)
+}
+
+// Construct Object fo manage my roll dice's Game.
+class ManageGame {
+
+    //game's default variable
+    static myGame = null
+    currentUser = 0
+    player = 1
+    scoreId = 'scorePlayer1'
+    playerZone = 'playerZone1'
+    playerWinner = 'playerWinner1'
+    scoreMax = 100
+
+    //game's principal class
+    static getNewgame(){
+        if(ManageGame.myGame === null) 
+            ManageGame.myGame = new ManageGame()
+        return ManageGame.myGame
+    }
+    constructor () {
+        this.players = []
+        this.players.push(new Player('Player 1'))
+        this.players.push(new Player('Player 2'))
+        this.myDice = new Dice()
+
+    }
+     // Reset Game 
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/** Modal option */
+const btnCloseModal = document.querySelector('.closeModal')
+const overlay = document.querySelector('.overlay')
+const modal = document.querySelector('.modal')
+
+function closeModal(){
+    modal.style.display = "none"
+    overlay.style.display = "none"
+}
+btnCloseModal.addEventListener('click', closeModal)
