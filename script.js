fed80e8866ecf8b78380b1361aff2ee90fb14d02
Ilan Tervil <ilan.tervil@wanadoo.fr>
2023-03-31 09:40:57 +0200

    22e commit back to old vers

--- script.js
+++ script.js
@@ -211,23 +211,23 @@ btnCloseModal.addEventListener('click', closeModal)
 // Test 3D
 
 
-function rollDice() {
-    const dice = [...document.querySelectorAll(".die-list")];
-    dice.forEach(die => {
-        toggleClasses(die);
-        die.dataset.roll = getRandomNumber(1, 6);
-    });
-}
+// function rollDice() {
+//     const dice = [...document.querySelectorAll(".die-list")];
+//     dice.forEach(die => {
+//         toggleClasses(die);
+//         die.dataset.roll = getRandomNumber(1, 6);
+//     });
+// }
 
-function toggleClasses(die) {
-    die.classList.toggle("odd-roll");
-    die.classList.toggle("even-roll");
-}
+// function toggleClasses(die) {
+//     die.classList.toggle("odd-roll");
+//     die.classList.toggle("even-roll");
+// }
 
-function getRandomNumber(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+// function getRandomNumber(min, max) {
+//     min = Math.ceil(min);
+//     max = Math.floor(max);
+//     return Math.floor(Math.random() * (max - min + 1)) + min;
+// }
 
-document.getElementById("roll-button").addEventListener("click", rollDice);
+// document.getElementById("roll-button").addEventListener("click", rollDice);
