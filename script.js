aa2e82646d0230b4c8291dba61d19fa326bbd9eb
Ilan Tervil <ilan.tervil@wanadoo.fr>
2023-04-11 16:49:02 +0200

    32e commit Refactorize of JS function

--- script.js
+++ script.js
@@ -45,7 +45,7 @@ class Dice {
 class ManageGame {
 
     //game's default variable
-    static myGame = null
+    static gameInstance = null
     currentUser = 0
     player = 1
     scoreId = 'scorePlayer1'
@@ -54,47 +54,47 @@ class ManageGame {
     scoreMax = 100
 
     resetGame() {
-          // Réinitialiser les valeurs du jeu
-        this.players.forEach(player => {
-            player.currentScore = 0;
-            player.totalScore = 0;
-        });
+        // Réinitialiser les valeurs des joueurs
+        for (let i = 0; i < this.players.length; i++) {
+            this.players[i].currentScore = 0;
+            this.players[i].totalScore = 0;
+        }
 
+        // Tableaux pour stocker les noms des éléments HTML
+        const scoreIds = ['scorePlayer1', 'scorePlayer2'];
+        const playerZones = ['playerZone1', 'playerZone2'];
+        const playerWinners = ['playerWinner1', 'playerWinner2'];
+        const currentScoreIds = ['currentScore1', 'currentScore2'];
+
+        // Réinitialiser les valeurs du jeu
         this.currentUser = 0;
         this.player = 1;
-        this.scoreId = 'scorePlayer1';
-        this.playerZone1 = 'playerZone1';
-        this.playerZone2 = 'playerZone2';
-        this.playerWinner1 = 'playerWinner1';
-        this.playerWinner2 = 'playerWinner2';
+        this.scoreId = scoreIds[0];
+        this.playerZone1 = playerZones[0];
+        this.playerZone2 = playerZones[1];
+        this.playerWinner1 = playerWinners[0];
+        this.playerWinner2 = playerWinners[1];
         this.scoreMax = 100;
 
-        // Réactiver le bouton Hold 
-
-        // Réactiver le bouton Hold - CSS Solution
+        // Réactiver le bouton Hold
         document.getElementById('myHoldButton').classList.remove('disabled');
 
         // Réinitialiser les éléments HTML
-        document.getElementById('scorePlayer1').innerText = 0;
-        document.getElementById('scorePlayer2').innerText = 0;
-        document.getElementById('currentScore1').innerText = 0;
-        document.getElementById('currentScore2').innerText = 0;
-        document.getElementById('playerName1').innerText = this.players[0].name;
-        document.getElementById('playerName2').innerText = this.players[1].name;
-        document.getElementById(this.playerZone1).classList.remove('confetti');
-        document.getElementById(this.playerZone2).classList.remove('confetti');
-        document.getElementById(this.playerWinner1).style.display = 'none';
-        document.getElementById(this.playerWinner2).style.display = 'none';
-        document.getElementById('currentScoreBox1').style.display = 'block';
-        document.getElementById('currentScoreBox2').style.display = 'block';
+        for (let i = 0; i < scoreIds.length; i++) {
+            document.getElementById(scoreIds[i]).innerText = 0;
+            document.getElementById(currentScoreIds[i]).innerText = 0;
+            document.getElementById('playerName' + (i + 1)).innerText = this.players[i].name;
+            document.getElementById(playerZones[i]).classList.remove('confetti');
+            document.getElementById(playerWinners[i]).style.display = 'none';
+            document.getElementById('currentScoreBox' + (i + 1)).style.display = 'block';
+        }
     }
-
 
     //Game's principal class
     static getNewGame() {
-        if (ManageGame.myGame === null)
-            ManageGame.myGame = new ManageGame()
-        return ManageGame.myGame
+        if (ManageGame.gameInstance === null)
+            ManageGame.gameInstance = new ManageGame()
+        return ManageGame.gameInstance
 
     }
     constructor() {
@@ -104,152 +104,159 @@ class ManageGame {
         this.myDice = new Dice()
     }
     
+
 // When I click on the dice
     onDiceClick() {
-
-        const currentPlayer = this.players[this.currentUser]
+        const currentPlayer = this.players[this.currentUser];
 
         if (currentPlayer.totalScore >= this.scoreMax) {
-            // Player has already won, do nothing
-            return
+            return;
         }
+
         this.myDice.rollDice();
 
-        if (this.myDice.myCurrentFace === 1) {
+        const currentFace = this.myDice.myCurrentFace;
+        const currentScoreElement = document.getElementById('currentScore' + this.player);
+        const playerNameElement = document.getElementById('playerName' + this.player);
+
+        if (currentFace === 1) {
             this.changeDice();
-            this.players[this.currentUser].currentScore = 0;
-            document.getElementById('currentScore' + this.player).innerText = this.currentPlayer.currentScore;
+            currentPlayer.currentScore = 0;
+            currentScoreElement.innerText = currentPlayer.currentScore;
             this.changeUser();
-        }
-        else {
-            this.players[this.currentUser].currentScore += this.myDice.myCurrentFace;
-            this.currentPlayer = this.players[this.currentUser];
-            document.getElementById('currentScore' + this.player).innerText = this.currentPlayer.currentScore;
-            document.getElementById('playerName' + this.player).innerText = this.currentPlayer.name;
+        } else {
+            currentPlayer.currentScore += currentFace;
+            this.currentPlayer = currentPlayer;
+            currentScoreElement.innerText = currentPlayer.currentScore;
+            playerNameElement.innerText = currentPlayer.name;
             this.changeDice();
 
-            if (this.currentPlayer.totalScore + this.currentPlayer.currentScore >= this.scoreMax) {
-                document.getElementById('totalScore' + this.player).innerText = this.currentPlayer.totalScore;
+            if (currentPlayer.totalScore + currentPlayer.currentScore >= this.scoreMax) {
+                document.getElementById('totalScore' + this.player).innerText = currentPlayer.totalScore;
             }
         }
     }
+
 
 /**when I click hold; add score in total score, and if the score is > or = 100, then it sets up 
    * PlayerWinner and puts confetti, else switch player */
 
     onHoldClick() {
-
-        let myHoldButton = document.querySelector('#myHoldButton')
-
         const currentPlayer = this.players[this.currentUser];
 
         currentPlayer.totalScore += currentPlayer.currentScore;
-        document.getElementById('scorePlayer' + this.player).innerText = currentPlayer.totalScore;
+        document.getElementById(`scorePlayer${this.player}`).innerText = currentPlayer.totalScore;
 
         if (currentPlayer.totalScore >= this.scoreMax) {
             // Player has won!
-            document.getElementById(this.playerZone).classList.add('confetti');
+            const playerZone = document.getElementById(this.playerZone);
+            playerZone.classList.add('confetti');
             document.getElementById(this.playerWinner).style.display = 'block';
-            document.getElementById('currentScoreBox' + this.player).style.display = 'none';
-            document.getElementById('playerWinner' + this.player).innerText = this.players[this.currentUser].name + ' has won !';
-
-            // Désactiver l'événement "click" du bouton
-            // myHoldButton.removeEventListener('click', this.onHoldClick);
+            const playerWinner = document.getElementById(`playerWinner${this.player}`);
+            playerWinner.innerText = `${this.players[this.currentUser].name} has won!`;
+            const currentScoreBox = document.getElementById(`currentScoreBox${this.player}`);
+            currentScoreBox.style.display = 'none';
 
             // Disable the Hold button - CSS Solution
+            const myHoldButton = document.querySelector('#myHoldButton');
             myHoldButton.classList.add('disabled');
-            
         } else {
             currentPlayer.currentScore = 0;
-            document.getElementById('currentScore' + this.player).innerText = currentPlayer.currentScore;
+            document.getElementById(`currentScore${this.player}`).innerText = currentPlayer.currentScore;
             this.changeUser();
         }
     }
 
+
+    // Change the current player
     changeUser() {
-        document.getElementById(this.playerZone).classList.remove('active')
-
-        if (this.currentUser === 0)
-            this.currentUser = 1
-        else this.currentUser = 0
-        this.player = this.currentUser + 1
-        this.scoreId = 'scorePlayer' + this.player
-        this.playerZone = 'playerZone' + this.player
-        this.playerWinner = 'playerWinner' + this.player
-
-        document.getElementById(this.playerZone).classList.add('active')
-
+        document.getElementById(this.playerZone).classList.remove('active');
+        this.currentUser = (this.currentUser + 1) % 2;
+        this.player = this.currentUser + 1;
+        this.scoreId = `scorePlayer${this.player}`;
+        this.playerZone = `playerZone${this.player}`;
+        this.playerWinner = `playerWinner${this.player}`;
+        document.getElementById(this.playerZone).classList.add('active');
     }
 
+    // Update the total score of the current player
     changeTotalScore() {
-        document.getElementById(this.scoreId).innerText = this.players[this.currentUser].totalScore
+        const currentPlayer = this.players[this.currentUser];
+        document.getElementById(this.scoreId).innerText = currentPlayer.totalScore;
     }
 
-    // but in place the random image of dice
+    // Change the dice image
     changeDice() {
-        document.getElementById('diceImage').classList.remove(this.getMyClassName(this.myDice.oldFace))
-        document.getElementById('diceImage').classList.add(this.getMyClassName(this.myDice.myCurrentFace))
+        const diceImage = document.getElementById('diceImage');
+        const oldClassName = this.getMyClassName(this.myDice.oldFace);
+        const newClassName = this.getMyClassName(this.myDice.myCurrentFace);
 
+        diceImage.classList.remove(oldClassName);
+        diceImage.classList.add(newClassName);
     }
+
 
 
     //sets the images in relation to the random
     getMyClassName(myNumberOfDice) {
-        if (myNumberOfDice === 1)
-            return 'one'
-        if (myNumberOfDice === 2)
-            return 'two'
-        if (myNumberOfDice === 3)
-            return 'three'
-        if (myNumberOfDice === 4)
-            return 'four'
-        if (myNumberOfDice === 5)
-            return 'five'
-        if (myNumberOfDice === 6)
-            return 'six'
+        const classMap = {
+            1: 'one',
+            2: 'two',
+            3: 'three',
+            4: 'four',
+            5: 'five',
+            6: 'six'
+        };
+
+        return classMap[myNumberOfDice];
     }
+
+
 }
+
 
 /** click on one of this three button for play */
 
-// event listener to "New Game" button
-document.getElementById('newGameButton').addEventListener('click', () => {
-    ManageGame.getNewGame().resetGame();
-});
+const buttons = [
+    { id: 'newGameButton', fn: 'resetGame' },
+    { id: 'myHoldButton', fn: 'onHoldClick' },
+    { id: 'diceButton', fn: 'onDiceClick' }
+];
 
-// add event listener to "Hold" button
-document.getElementById('myHoldButton').addEventListener('click', () => {
-    ManageGame.getNewGame().onHoldClick();
-});
-
-
-
-// add event listener to "dice" button
-document.getElementById('diceButton').addEventListener('click', () => {
-    ManageGame.getNewGame().onDiceClick();
+buttons.forEach(button => {
+    document.getElementById(button.id).addEventListener('click', () => {
+        ManageGame.getNewGame()[button.fn]();
+    });
 });
 
 
 /** Modal option */
-const btnCloseModal = document.querySelector('.closeModal')
-const overlay = document.querySelector('.overlay')
-const modal = document.querySelector('.modal')
 
-const btnOpenModal = document.querySelector('.rules')
+// Fonction pour afficher ou masquer un élément
+function toggleElement(element, show) {
+    element.style.display = show ? "" : "none";
+}
+
+// Éléments HTML
+const btnCloseModal = document.querySelector('.closeModal');
+const overlay = document.querySelector('.overlay');
+const modal = document.querySelector('.modal');
+const btnOpenModal = document.querySelector('.rules');
 
 // Btn Closed
-function closeModal(){
-    modal.style.display = "none"
-    overlay.style.display = "none"
+function closeModal() {
+    toggleElement(modal, false);
+    toggleElement(overlay, false);
 }
-btnCloseModal.addEventListener('click', closeModal)
+btnCloseModal.addEventListener('click', closeModal);
 
 // Btn Open
-function OpenModal() {
-    modal.style.display = ""
-    overlay.style.display = ""
+function openModal() {
+    toggleElement(modal, true);
+    toggleElement(overlay, true);
 }
-btnOpenModal.addEventListener('click', OpenModal)
+btnOpenModal.addEventListener('click', openModal);
+
 
 
 // TODO
@@ -258,6 +265,7 @@ btnOpenModal.addEventListener('click', OpenModal)
  * OK - onDiceClick - Corriger la valeur de fin, quand on ajoute notre résultat final, avant d'atteindre 100, qui s'addition avec 100, au lieu d'atteindre 100 tout juste
  * OK - En HTML : Modal & Game Rules, mieux faire (en utilisant Bootstrap ?) les règles, qu'elles soient plus claires.
  * OK - Ajout en HTML,CSS et JS, d'un bouton Rules, pour revoir la modal Game Rules.
+ * - Refactoriser le code.
  */
 
 
